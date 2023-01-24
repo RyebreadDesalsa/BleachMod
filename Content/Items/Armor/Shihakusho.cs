2c590f6cf82c7eb6f25c4505e042d0759316f8c9
RyebreadDesalsa <rde632@gmail.com>
2023-01-24 12:30:56 -0500

    Reworked the basic Shinigami armor

--- Content/Items/Armor/Shihakusho.cs
+++ Content/Items/Armor/Shihakusho.cs
@@ -27,23 +27,7 @@ namespace BleachMod.Content.Items.Armor
 			Item.width = 18;
 			Item.height = 14;
 			Item.rare = ItemRarityID.Blue;
-			Item.defense = 4;
-		}
-
-        public override void UpdateEquip(Player player){
-			
-		}
-		// IsArmorSet determines what armor pieces are needed for the setbonus to take effect
-		public override bool IsArmorSet(Item head, Item body, Item legs)
-		{
-			return body.type == ModContent.ItemType<Shihakusho>() && legs.type == ModContent.ItemType<Shinigami_Sandals>();
-		}
-
-		// UpdateArmorSet allows you to give set bonuses to the armor.
-		public override void UpdateArmorSet(Player player)
-		{
-			player.setBonus = "Increases spiritual pressure by 100";
-			player.GetModPlayer<BleachPlayer>().MaxPressure += 100;
+			Item.defense = 6;
 		}
 
 		public override void SetMatch(bool male, ref int equipSlot, ref bool robes) {
---
+++ Content/Items/Armor/Squad_Member.cs
@@ -0,0 +1,57 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using BleachMod.Common.Players;
+
+namespace BleachMod.Content.Items.Armor
+{
+
+	[AutoloadEquip(EquipType.Head)]
+	public class Squad_Member : ModItem
+	{
+		public override void Load()
+		{
+			if (Main.netMode == NetmodeID.Server)
+			{
+				return;
+			}
+		}
+
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Squad Member Rank");
+			Tooltip.SetDefault("The standard rank of a Shinigami.");
+            ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 14;
+			Item.rare = ItemRarityID.Blue;
+			Item.defense = 5;
+		}
+
+		// IsArmorSet determines what armor pieces are needed for the setbonus to take effect
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return head.type == ModContent.ItemType<Squad_Member>() && body.type == ModContent.ItemType<Shihakusho>() && legs.type == ModContent.ItemType<Shinigami_Sandals>();
+		}
+
+		// UpdateArmorSet allows you to give set bonuses to the armor.
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = "Increases spiritual pressure by 100";
+			player.GetModPlayer<BleachPlayer>().MaxPressure += 100;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Silk, 10);
+			recipe.AddTile(TileID.Loom);
+			recipe.Register();
+		}
+	}
+}
