9fa612877e06dc8140b74b26054bdf6cbce391f8
RyebreadDesalsa <rde632@gmail.com>
2023-10-01 14:08:50 -0400

    Reworked Flash Step

--- Common/Players/FlashStepKeybindPlayer.cs
+++ Common/Players/FlashStepKeybindPlayer.cs
@@ -19,30 +19,21 @@ namespace BleachMod.Common.Players
 			{
 				if (Player.GetModPlayer<BleachPlayer>().C_Pressure >= 50 && (Player.GetModPlayer<BleachPlayer>().hasBadge || Player.GetModPlayer<BleachPlayer>().hasCloak))
 				{
-					Microsoft.Xna.Framework.Vector2 tploc = default(Microsoft.Xna.Framework.Vector2);
-					tploc.X = (float)Main.mouseX + Main.screenPosition.X;
-					if (Player.gravDir == 1f)
-					{
-						tploc.Y = (float)Main.mouseY + Main.screenPosition.Y - (float)Player.height;
-					}
-					else
-					{
-						tploc.Y = Main.screenPosition.Y + (float)Main.screenHeight - (float)Main.mouseY;
-					}
-					tploc.X -= Player.width / 2;
-					this.LimitPointToPlayerReachableArea(ref tploc);
-					if (!(tploc.X > 50f) || !(tploc.X < (float)(Main.maxTilesX * 16 - 50)) || !(tploc.Y > 50f) || !(tploc.Y < (float)(Main.maxTilesY * 16 - 50)))
-					{
-						return;
-					}
-					int num = (int)(tploc.X / 16f);
-					int num2 = (int)(tploc.Y / 16f);
-					if (!((Main.tile[num, num2].WallType == 87 && (double)num2 > Main.worldSurface && !NPC.downedPlantBoss) || Collision.SolidCollision(tploc, Player.width, Player.height)))
-					{
-						Player.Teleport(tploc, 3);
-						NetMessage.SendData(MessageID.TeleportEntity, -1, -1, null, 0, Player.whoAmI, tploc.X, tploc.Y, 1);
-						Player.GetModPlayer<BleachPlayer>().C_Pressure -= 50;
-					}
+					Player.GetModPlayer<BleachPlayer>().C_Pressure -= 50;
+					Vector2 playerPos = Player.Center;
+					Vector2 mousePos = Main.MouseWorld;
+					int spdmod =(int) (Player.GetModPlayer<BleachPlayer>().MaxPressure/ 8);
+					if (spdmod > 30)
+                    {
+						spdmod = 30;
+                    }
+
+					Vector2 direction = mousePos - playerPos;
+					direction.Normalize();
+					direction = direction * spdmod;
+					
+					Player.velocity = direction;
+					
 				}
 			}
 			
